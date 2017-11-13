85896d23909bcdf0f5e1f3cac97498d1e703b6ed
COsborn2 <cameron0401@gmail.com>
2017-11-13 15:40:06 -0800

    Update Commands.h

--- server/commands.h
+++ server/commands.h
@@ -2,10 +2,12 @@
 #define commands_h
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
+int isCommand(char * toParse);
 int isValidCommand(char * toParse);
 void makeLowerCase(char * toParse);
 void executeCommand(char * toParse);
