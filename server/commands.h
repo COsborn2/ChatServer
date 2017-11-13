153ec67b5193d7fde1d9b6b02d53a5cc11d8b1cc
COsborn2 <cameron0401@gmail.com>
2017-11-13 15:27:31 -0800

    Complete basic logic. Need to add actions.

--- server/commands.h
+++ server/commands.h
@@ -1,6 +1,14 @@
 #ifndef commands_h
 #define commands_h
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+int isValidCommand(char * toParse);
+void makeLowerCase(char * toParse);
+void executeCommand(char * toParse);
+void removeSpaces(char * toParse);
 
 #endif
