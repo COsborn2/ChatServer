082921bba10bb0fac5432e0c7ad78d774811e637
Kenneth White <kennival@hotmail.com>
2017-11-18 17:45:37 -0800

    Implemented Private chat
    Added logic for private chats.

--- server/commands_exec.c
+++ server/commands_exec.c
@@ -26,3 +26,70 @@ void executeHelp(const int cur, const Client *clients, Message *message) {
 	updateAndWriteMessage(clients[cur].sockedfd, message, LANG_HELP_8);
 	updateAndWriteMessage(clients[cur].sockedfd, message, LANG_HELP_9);
 }
+
+/*
+ * Opens a private chat between cur and specified user
+ */
+void executePChat(const int cur, Client *clients, Message *message) {
+
+    //Client already in priv chat
+    if(clients[cur].privChat >= 0){
+        dprintf(clients[cur].sockedfd, "\nServer: You are already in a private chat, "
+                "please end the private\n chat using '/q' before connecting to a new client.\n");
+        return;
+    }
+
+
+    //increment past '/r '
+	char* temp;
+	strcpy(temp, message->data);
+	if(temp[2] == ' '){temp = temp +3;}
+	else{temp = temp + 2;}
+
+
+
+
+	//sets privChat to 1 in both client, adds index in client for priv_chats array
+	int x = 0;
+	for(; x < MAXCLIENTS; x++){
+		if(strcmp(temp, clients[x].name) == 0){
+            if(clients[x].privChat >= 0){
+                dprintf(clients[cur].sockedfd, "\nServer: User %s is already in a private chat.\n", clients[x].name);
+                return;
+            }
+			clients[x].privChat = clients[cur].sockedfd;
+			clients[cur].privChat = clients[x].sockedfd;
+			return;
+		}
+	}
+	//failed to create private chat
+	dprintf(clients[cur].sockedfd, "\nServer: Could not find user with that name.\n");
+}
+
+void executeEndPChat(const int cur, Client *clients) {
+
+	//not in a priv chat
+	if(clients[cur].privChat < 0){
+		dprintf(clients[cur].sockedfd, "Server: You are not connected to a private chat. \n");
+		return;
+	}
+
+	//sets privChat to 0 in both client, sets inUse to 0 in priv_Chats[]
+	int x = 0;
+	int cliOther = clients[cur].privChat;
+
+	for(; x < MAXCLIENTS; x++){
+		//remove clients from priv chat
+		if(clients[x].sockedfd == cliOther) {
+			clients[x].privChat = -1;
+			clients[cur].privChat = -1;
+
+			//inform both clients
+			dprintf(clients[x].sockedfd, "\nServer: Disconnected from private chat with %s.\n",clients[cur].name );
+			dprintf(clients[cur].sockedfd, "\nServer: Disconnected from private chat with %s.\n",clients[x].name );
+            return;
+		}
+	}
+	//failed to quit private chat
+	dprintf(clients[cur].sockedfd, "\nServer: Could not find user with that name.\n");
+}
--- server/server.h
+++ server/server.h
@@ -2,6 +2,16 @@
 #define SERVER_H
 
 #include <sys/select.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include "commands.h"
+#include "lang.h"
 
 #define MAXCLIENTS 30
 #define MAX 100
@@ -17,6 +27,7 @@ typedef struct {
     int sockedfd;
     char name[MAX_NAME];
     int roomNumber;
+	int privChat;
 } Client;
 
 
@@ -35,6 +46,7 @@ extern const Room roomNetwork;
 extern const int default_room_count;
 extern const Room *def_rooms[2];
 
+
 void clearMessage(Message *message);
 void updateMessage(Message *message, const char *nstring);
 void writeMessage(int commfd, Message *message);
@@ -43,6 +55,7 @@ void updateAndWriteMessage(int commfd, Message *message, const char *nstring);
 
 void printToAll(const Client *clients, Message *message);
 void printToOthersInRoom(const Client *clients, int cur, Message *message);
+void printToPrivChat(Client *clients, int cur,  Message * message);
 void disconnectClient(int cur, Client *clients, Message *sendMessage);
 
 #endif
